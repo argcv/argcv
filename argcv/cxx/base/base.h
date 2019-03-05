c67a6cbeff7190e4e1034cbe69749321909d586b
Yu Jing <yuikns@users.noreply.github.com>
2019-03-05 02:15:01 -0500

    add status

--- argcv/cxx/base/base.h
+++ argcv/cxx/base/base.h
@@ -2,6 +2,8 @@
 #ifndef ARGCV_CXX_BASE_BASE_H_
 #define ARGCV_CXX_BASE_BASE_H_
 
+#include "argcv/cxx/base/macros.h"
+#include "argcv/cxx/base/status.h"
 #include "argcv/cxx/base/types.h"
 
 #endif  // ARGCV_CXX_BASE_BASE_H_
---
+++ argcv/cxx/base/status.h
@@ -0,0 +1,7 @@
+// Copyright 2019 Yu Jing
+#ifndef ARGCV_CXX_BASE_STATUS_H_
+#define ARGCV_CXX_BASE_STATUS_H_
+
+namespace argcv {}
+
+#endif  // ARGCV_CXX_BASE_STATUS_H_
